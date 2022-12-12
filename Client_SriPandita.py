ca4f43cdd37d1818b412f9f6ab0a0bbb505f7ad6
‎‎‎‎‎‎NakHalal <haidaruddinmuhammadr@gmail.com>
2022-12-12 15:55:19 +0700

    0.0.1

---
+++ Client_SriPandita.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''Client dari Program FTP'''
+__author__ = "Haidaruddin Muhammad Ramdhan"
+__copyright__ = "© SriPandita 2022"
+__credits__ = ["GitHub",
+               "VisualStudioCode"]
+__license__ = "GNU General Public License v3.0"
+__version__ = "0.0.1"
+__maintainer__ = ["Muhammad Dimas Rifki Irianto",
+                  "Ahmad Fasya Adila",
+                  "Haidaruddin Muhammad Ramdhan",
+                  "Muhammad Hiksal Daeng Jusuf Bauw"]
+__email__ = "haidaruddinmuhammadr@gmail.com"
+__status__ = "Production"
