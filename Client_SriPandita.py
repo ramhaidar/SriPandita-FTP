510a0a8f7da93488d067a15ceda7b17b500ddb93
‎‎‎‎‎‎NakHalal <haidaruddinmuhammadr@gmail.com>
2022-12-13 16:11:56 +0700

    0.0.2
    Successful file transfer from client to server.

--- Client_SriPandita.py
+++ Client_SriPandita.py
@@ -3,7 +3,8 @@
 __author__ = "Haidaruddin Muhammad Ramdhan"
 __copyright__ = "© SriPandita 2022"
 __credits__ = ["GitHub",
-               "VisualStudioCode"]
+               "VisualStudioCode",
+               "https://github.com/justEhmadSaeed/Python-Sockets-File-Transfer"]
 __license__ = "GNU General Public License v3.0"
 __version__ = "0.0.1"
 __maintainer__ = ["Muhammad Dimas Rifki Irianto",
@@ -12,3 +13,24 @@ __maintainer__ = ["Muhammad Dimas Rifki Irianto",
                   "Muhammad Hiksal Daeng Jusuf Bauw"]
 __email__ = "haidaruddinmuhammadr@gmail.com"
 __status__ = "Production"
+
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 48632
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+file = open('MyFile.bin', 'rb')
+line = file.read(1024)
+
+while (line):
+    s.send(line)
+    line = file.read(1024)
+
+file.close()
+print('File has been transferred successfully.')
+
+s.close()
--- Server_SriPandita.py
+++ Server_SriPandita.py
@@ -3,7 +3,8 @@
 __author__ = "Haidaruddin Muhammad Ramdhan"
 __copyright__ = "© SriPandita 2022"
 __credits__ = ["GitHub",
-               "VisualStudioCode"]
+               "VisualStudioCode",
+               "https://github.com/justEhmadSaeed/Python-Sockets-File-Transfer"]
 __license__ = "GNU General Public License v3.0"
 __version__ = "0.0.1"
 __maintainer__ = ["Muhammad Dimas Rifki Irianto",
@@ -12,3 +13,40 @@ __maintainer__ = ["Muhammad Dimas Rifki Irianto",
                   "Muhammad Hiksal Daeng Jusuf Bauw"]
 __email__ = "haidaruddinmuhammadr@gmail.com"
 __status__ = "Production"
+
+import socket
+import os
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 48632
+BUFFER_SIZE = 1024
+
+UserName = "ContohUserX"
+NamaFile = "MyFile.bin"
+
+CheckThis = ("Database\\" + UserName)
+
+if (os.path.exists(CheckThis) == False):
+    os.mkdir(CheckThis)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+while 1:
+    conn, addr = s.accept()
+
+    SaveTo = ("Database\\" + UserName + "\\" + NamaFile)
+    print(SaveTo)
+    file = open(SaveTo, 'wb')
+    line = conn.recv(BUFFER_SIZE)
+
+    while (line):
+        file.write(line)
+        line = conn.recv(BUFFER_SIZE)
+
+    file.close()
+    conn.close()
+    s.listen(0)
+    s.close()
+    break
